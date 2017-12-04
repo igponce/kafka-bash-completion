f408104354dfa64d9752e4bff591f21903f4bee6
Iñigo Gonzalez <C:\Users\Iigo\AppData\Roaming\The Bat!>
2017-12-04 00:17:20 +0000

    fka-topics --config autocomplete options available (for kafka up to 1.0)

--- kafka-bash.sh
+++ kafka-bash.sh
@@ -1,11 +1,10 @@
 # Kafka Bash Completion
 # (c) 2017 Iñigo Gonzalez Ponce
 # This is unreleased propietary software (by now)
-
+#
 # We have one function for every command that provides autocompletion.
 # The function that configures autocompletion has the same name of the
 # command preceded by __ (double underscore)
-
 function __kafka_topics_sh () {
 
     
@@ -14,10 +13,10 @@ function __kafka_topics_sh () {
     
     local cur=${COMP_WORDS[COMP_CWORD]}
 
-    if [ "X$2" == "--config"] ; then
-        COMPREPLY=($(IFS='|' compgen -S ' ' -W "$topics_configs" -- $cur ) )
+    if [ "X$3" == "X--config" ] ; then
+        COMPREPLY=($(IFS='|' compgen -S ' ' -W "$topic_configs" -- $cur ) )
     else
-       COMPREPLY=($(IFS='|' compgen -S ' ' -W "$base_topics_ops" -- $cur ) )
+        COMPREPLY=($(IFS='|' compgen -S ' ' -W "$base_topics_ops" -- $cur ) )
     fi
 
 }
